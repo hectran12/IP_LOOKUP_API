199478e5ee8672dd3177e4cd28ebf48ef177d1c7
Trần Trọng Hòa <86024288+hexzzz2008@users.noreply.github.com>
2022-06-28 12:49:19 +0700

    Create ip_lookup_axios.js

---
+++ ip_lookup_axios.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+
+const response = await axios.get('https://tronghoa.dev/api/ip_lookup.php', {
+    params: {
+        'ipadress': 'me'
+    },
+    headers: {
+        'authority': 'tronghoa.dev',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'accept-language': 'en-US,en;q=0.9',
+        'referer': 'https://tronghoa.dev/',
+        'sec-ch-ua': '" Not;A Brand";v="99", "Microsoft Edge";v="103", "Chromium";v="103"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'document',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.53 Safari/537.36 Edg/103.0.1264.37'
+    }
+});
